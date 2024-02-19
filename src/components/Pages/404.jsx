e352f11b075836d863fc53c08f12bd5c95ac119e
Beata Konya <beatakonya@gmail.com>
2024-02-19 23:24:58 +0000

    added 404 page

---
+++ src/components/Pages/404.jsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+    return (
+        <>
+        <h1>404 Error. Page is under construction. Please come back later.</h1>
+        </>
+    )
+}
+
+export default Custom404;
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,3 +1,4 @@
+export { default as Costum404  } from './Costum404';
 export { default as Contact } from './Contact';
 export { default as Home } from './Home';
 export { default as ProjectGallery } from './ProjectGallery'
