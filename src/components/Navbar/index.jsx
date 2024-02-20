fdfd92ad5c619a9bbe1256bb893c52dfea59eb07
Beata Konya <beatakonya@gmail.com>
2024-02-20 10:01:59 +0000

    changed logo brand text and title

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,7 +7,7 @@ function MyNav() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#">Beata Konya's Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
